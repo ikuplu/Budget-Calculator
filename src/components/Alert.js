c8e8cdcc7d75b99c8502a255b02122e7448d8b7d
Ismail Kuplu <i.kuplu@outlook.com>
2021-08-07 17:44:13 +0200

    Created main component files within a seperate folder

---
+++ src/components/Alert.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Alert = () => {
+  return <div>Hello from alert</div>;
+};
+
+export default Alert;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ExpenseList = () => {
+  return <div>Hello from expense list</div>;
+};
+
+export default ExpenseList;
