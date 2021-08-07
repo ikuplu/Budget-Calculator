5060875a04e866d256db9bc0fa495509409f6f95
Ismail Kuplu <i.kuplu@outlook.com>
2021-08-07 19:17:39 +0200

    Matched the expenses state with the expenses prop of the ExpenseList component and rendered the total expense to the screen by using reduce function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ExpenseList from './components/ExpenseList';
 import ExpenseForm from './components/ExpenseForm';
 import Alert from './components/Alert';
 import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 
 const initialExpenses = [
   { id: uuidv4(), charge: 'rent', amount: 1000 },
@@ -10,14 +11,26 @@ const initialExpenses = [
   { id: uuidv4(), charge: 'credit card bill', amount: 1200 },
 ];
 
-console.log(initialExpenses);
-
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
   return (
     <>
       <Alert />
-      <ExpenseForm />
-      <ExpenseList />
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        total spending:{' '}
+        <span className="total">
+          ${' '}
+          {expenses.reduce((acc, curr) => {
+            return (acc += curr.amount);
+          }, 0)}
+        </span>
+      </h1>
     </>
   );
 }
